31cab7c26d0674318776a2f8c46f93be014ecc3e
chandle greff <unconfigured@null.spigotmc.org>
2022-07-18 23:04:55 +0200

    Added react

---
+++ resources/js/app.jsx
@@ -0,0 +1,6 @@
+import "./bootstrap";
+import "../css/app.css";
+import ReactDOM from "react-dom/client";
+import Home from "./components/Home";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Home />);
