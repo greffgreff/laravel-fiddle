a70ce6ffd69b65b0de7ab2b574eb6ba1f8d441b8
chandle greff <unconfigured@null.spigotmc.org>
2022-07-19 08:03:27 +0200

    Added textarea input

--- resources/js/components/AddForm.jsx
+++ resources/js/components/AddForm.jsx
@@ -1,4 +1,4 @@
-import { faPlus, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faPlus, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
 import Draggable from 'react-draggable'
@@ -16,9 +16,15 @@ export default function AddForm() {
             {form ? (
                 <Draggable>
                     <div className="form-container">
-                        <button className="form-close-btn" onClick={() => showForm(false)}>
-                            <FontAwesomeIcon icon={faXmark} />
-                        </button>
+                        <div className="form-container-btns">
+                            <button className="form-btn" onClick={() => showForm(false)}>
+                                <FontAwesomeIcon icon={faXmark} />
+                            </button>
+                            <button className="form-btn" onClick={() => showForm(false)}>
+                                <FontAwesomeIcon icon={faCheck} />
+                            </button>
+                        </div>
+                        <textarea className="todo-input" placeholder="Beat the shit out of someone..." />
                     </div>
                 </Draggable>
             ) : null}
