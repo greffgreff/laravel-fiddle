d6e420ed2b9e2da56d09aac4ebc1e1d2a31d44c9
chandle greff <unconfigured@null.spigotmc.org>
2022-07-20 12:22:04 +0200

    Added put call on change

--- resources/js/components/ToDoItem.jsx
+++ resources/js/components/ToDoItem.jsx
@@ -1,32 +1,32 @@
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
-import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 import '../../css/ToDoItem.css';
 
-export default function ToDoItem({ title, checked = false, dateCreated }) {
+export default function ToDoItem({ id, title, checked = false, dateCreated, onChange }) {
     const [complete, isComplete] = useState(checked);
 
     const handleComplete = () => {
         isComplete(!complete);
-        if (!complete) {
-            confetti();
-        }
+        const todoToUpdate = {
+            id: id,
+            status: !complete ? 1 : 0,
+        };
+
+        axios
+            .put('/updateTodoStatus', todoToUpdate)
+            .then((res) => onChange(res.data))
+            .catch(console.log);
     };
 
     return (
         <div className="todo-item-container" onClick={handleComplete}>
-            <input
-                className="todo-checkbox"
-                type="checkbox"
-                checked={complete}
-                onChange={() => null} // remove warning
-            />
+            <input className="todo-checkbox" type="checkbox" checked={complete} readOnly />
             <div className="todo-item-title" style={{ opacity: complete ? 0.2 : 1 }}>
                 {title}
             </div>
             {dateCreated ? (
-                <span class="tooltip-text">
+                <span className="tooltip-text">
                     <FontAwesomeIcon icon={faInfoCircle} />
                     Added {dateCreated}
                 </span>
